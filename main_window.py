6adcce7b457c57a2156ad4fa07ad69faf4da3364
Tommy <8moonx@gmail.com>
2022-07-24 19:45:17 +0200

    Added browse path buttons without functions

--- main_window.py
+++ main_window.py
@@ -1,3 +1,5 @@
+import configparser
+from distutils.command.config import config
 import tkinter
 from tkinter import filedialog
 
@@ -10,9 +12,18 @@ def close_window():
     main.destroy()
 
 # function for browsing directory path
-def open_directory():
+def open_directory(button):
     directory_path = filedialog.askdirectory()
-    print(directory_path)
+    # write directory path into launcher_options.ini
+    config = configparser.ConfigParser()
+    config.read('launcher_options.ini')
+    #config['GAMEPATH']['BFMEII'] = directory_path
+    if button.cget("text") == "browse_path_1":
+        config['GAMEPATH']['BFMEII'] = directory_path 
+    elif button.cget("text") == "browse_path_2":
+        config['GAMEPATH']['BFMEIIROTWK'] = directory_path
+    with open('launcher_options.ini', 'w') as configfile:
+        config.write(configfile)
 
 #--------------------------#
 # main window with buttons #
@@ -22,10 +33,14 @@ def open_directory():
 main = tkinter.Tk()
 
 # button browse path for BFME II
-browse_path_1 = tkinter.Button(main, text = "Browse", command=open_directory)
+browse_path_1 = tkinter.Button(main, text = "browse_path_1")
+browse_path_1.config(command = lambda button = browse_path_1 : open_directory(button))
 browse_path_1.pack()
 
 # button browse path for BFME II ROTWK
+browse_path_2 = tkinter.Button(main, text = "browse_path_2")
+browse_path_2.config(command = lambda button = browse_path_2 : open_directory(button))
+browse_path_2.pack()
 
 # button close window
 button_close = tkinter.Button(main, text = "Close", command = close_window)
