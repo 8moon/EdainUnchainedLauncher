d588f9e529db2f6dae6fa5ec3d31fe8469bfcb9a
Tommy <8moonx@gmail.com>
2022-07-25 21:52:26 +0200

    progress on installation/update function

--- main_window.py
+++ main_window.py
@@ -1,8 +1,15 @@
-from cProfile import label
 import configparser
-from distutils.command.config import config
+import os
+import sys
 import tkinter
+from cProfile import label
+from distutils.command.config import config
 from tkinter import RAISED, Label, StringVar, filedialog
+import urllib.request
+import shutil
+import zipfile
+import requests
+import gdown
 
 #------------------#
 # global variables #
@@ -34,6 +41,23 @@ def read_launcher_options(section, subsection):
     config = configparser.ConfigParser()
     config.read('launcher_options.ini')
     return config[section][subsection]
+
+# function for installing or updating edain unchained with source from download repository
+def install_mod():
+    # check if mod is installed in latest version --> TO DO
+    # check if temp folder for installation/update exists and delete if needed --> TO DO
+    # create temp folder for installation/update --> TO DO
+    # download folder content from google drive into temp folder --> TO DO
+    folder_url = r'https://drive.google.com/drive/folders/1iAZZdiWQxQFZpdez8MFu1TLl0y2lNB-s'
+    gdown.download_folder(url=folder_url, output=r'C:\Users\thoma\Desktop\Edain Unchained Test files', quiet=False, use_cookies=False)
+    # unzip files into temp directory --> TO DO
+    zipfile_path = r'C:\Users\thoma\Desktop\Edain Unchained Test files\file_01.zip'
+    target_zipfile_path = r'C:\Users\thoma\Desktop\Edain Unchained Test files\bfmeii folder'
+    with zipfile.ZipFile(zipfile_path, 'r') as zip_ref:
+        zip_ref.extractall(target_zipfile_path)
+    # move override unzipped files from temp folder into target directory --> TO DO
+    # cleanup temp directory afterwards --> TO DO
+    os.remove(zipfile_path) if os.path.exists(zipfile_path) else print("zipfile_path does not exist")
 
 #--------------------------#
 # main window with buttons #
@@ -80,7 +104,7 @@ label_bfmeiirotwk.pack()
 # ___________________harad_art.big goes into bfmeiirotwk folder
 install_edain_unchained_button_text = tkinter.StringVar()
 install_edain_unchained_button_text.set("Update") if 1 > 0 else install_edain_unchained_button_text.set("Install")
-install_edain_unchained = tkinter.Button(main, textvariable = install_edain_unchained_button_text)
+install_edain_unchained = tkinter.Button(main, textvariable = install_edain_unchained_button_text, command = install_mod)
 install_edain_unchained.pack()
 
 # button close window
